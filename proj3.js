4cbe1c217e9d1c3b7710931f58cebeba2e7afeb4
ahui9091 <ahui9091@gmail.com>
2026-02-14 11:05:03 -0600

    finish mini prjec

--- proj3.js
+++ proj3.js
@@ -1,15 +1,18 @@
 function setup() {
   createCanvas(400, 400);
-  background(0);
+  background(35);
 }
 
-function draw() {
-  
-  noStroke(0);
-  fill(200);
-  circle(mouseX, mouseY, 20)
+  function draw() {
+    noStroke(0);
+    fill(200, 2, 2);
+    circle(mouseX, mouseY, 20)
+    let mousepress = mouseIsPressed;
+    if (mousepress == true){
+        noStroke(0);
+        fill(100, 35, 35);
+        circle(mouseX, mouseY, 20)
+    }
 }
 
-function mousePressed() {
-  background(0);
-}
+
